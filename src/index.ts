0bacd56a8f17eb3e00ab5ed5bb0d28af06dd0ead
Jose Baena <jdanielbaena@hotmail.com>
2025-06-02 22:24:45 -0500

    Add Apache License, README, and update configuration files for create-env-file task

--- src/index.ts
+++ src/index.ts
@@ -3,49 +3,56 @@ import * as path from "path";
 import * as tl from "azure-pipelines-task-lib/task";
 
 async function run() {
-try {
-    const groupName = tl.getInput('variableGroupName', true);
-    const outputPath = tl.getInput('outputPath', true);
-    const token = tl.getVariable("System.AccessToken")!;
-    const project = tl.getVariable("System.TeamProject")!;
-    const orgUrl = tl.getVariable("System.CollectionUri")!;
+    try {
+        const groupName = tl.getInput('variableGroupName', true);
+        const outputPath = tl.getInput('outputPath', true);
+        const token = tl.getVariable("System.AccessToken")!;
+        const project = tl.getVariable("System.TeamProject")!;
+        const orgUrl = tl.getVariable("System.CollectionUri")!;
 
-    if (!token) {
-    throw new Error('System.AccessToken not available. Enable "Allow scripts to access OAuth token".');
+        if (!token) {
+            throw new Error('System.AccessToken not available. Enable "Allow scripts to access OAuth token".');
+        }
+
+        const apiUrl = `${orgUrl}${project}/_apis/distributedtask/variablegroups?api-version=7.1-preview.2`;
+
+        const response = await fetch(apiUrl, {
+        method: 'GET',
+        headers: {
+            'Authorization': `Bearer ${token}`,
+            'Accept': 'application/json'
+        }
+        });
+
+        if (!response.ok) {
+            throw new Error(`Error fetching Variable Groups: ${response.status} ${response.statusText}`);
+        }
+
+        const data = await response.json();
+        const groups = data.value;
+        const group = groups.find(g => g.name === groupName);
+
+        if (!group) {
+            throw new Error(`Variable Group "${groupName}" not found in the project "${project}"`);
+        }
+
+        const lines = Object.entries(group.variables as Record<string, { value: string }>)
+            .map(([key]) => {
+                const value = tl.getVariable(key);
+                if (value === undefined) {
+                    throw new Error(`The variables "${key}" is undefined. Make sure the Variable Group ${groupName} is linked/imported in your pipeline YAML or Pipeline Clasic.`);
+                }
+                return `${key}="${tl.getVariable(key)}"`;
+            });
+
+            
+        const fullPath = path.resolve(outputPath);
+        fs.writeFileSync(fullPath, lines.join('\n'), 'utf8');
+
+        tl.setResult(tl.TaskResult.Succeeded, `Environment file created at: ${fullPath}`);
+        } catch (err) {
+            tl.setResult(tl.TaskResult.Failed, err.message);
     }
-
-    const apiUrl = `${orgUrl}${project}/_apis/distributedtask/variablegroups?api-version=7.1-preview.2`;
-
-    const response = await fetch(apiUrl, {
-    method: 'GET',
-    headers: {
-        'Authorization': `Bearer ${token}`,
-        'Accept': 'application/json'
-    }
-    });
-
-    if (!response.ok) {
-    throw new Error(`Error fetching Variable Groups: ${response.status} ${response.statusText}`);
-    }
-
-    const data = await response.json();
-    const groups = data.value;
-    const group = groups.find(g => g.name === groupName);
-
-    if (!group) {
-    throw new Error(`Variable Group "${groupName}" not found in the project "${project}"`);
-    }
-
-    const lines = Object.entries(group.variables as Record<string, { value: string }>)
-    .map(([key, obj]) => `${key}="${tl.getVariable(key)}"`);
-
-    const fullPath = path.resolve(outputPath);
-    fs.writeFileSync(fullPath, lines.join('\n'), 'utf8');
-
-    tl.setResult(tl.TaskResult.Succeeded, `Environment file created at: ${fullPath}`);
-} catch (err) {
-    tl.setResult(tl.TaskResult.Failed, err.message);
-}
 }
  
- run();
+run();
